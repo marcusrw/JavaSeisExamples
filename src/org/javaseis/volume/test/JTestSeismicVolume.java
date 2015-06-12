dc3c6c3a72c41223d4b5de91fe7726281d88f0ed
Marcus Wilson <wilsonmr@ogeop.com>
2015-06-11 20:59:08 -0600

    Added placeholder test so ant wouldn't complain about a JTest with no executable methods

--- src/org/javaseis/volume/test/JTestSeismicVolume.java
+++ src/org/javaseis/volume/test/JTestSeismicVolume.java
@@ -1,5 +1,13 @@
 package org.javaseis.volume.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class JTestSeismicVolume {
 
+  @Test
+  public void PlaceHolder() {
+    Assert.assertTrue(true);
+  }
 }
