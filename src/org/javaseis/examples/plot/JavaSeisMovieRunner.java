3f93e8697450870c41fdf2d3a9a2c4deb93ecfc4
Marcus Wilson <wilsonmr@ogeop.com>
2015-06-15 19:32:01 -0600

    Added testFFT to example datasets

--- src/org/javaseis/examples/plot/JavaSeisMovieRunner.java
+++ src/org/javaseis/examples/plot/JavaSeisMovieRunner.java
@@ -15,9 +15,8 @@ public class JavaSeisMovieRunner {
   public static void main(String[] args) {
     //String pathToDataset = "/home/wilsonmr/javaseis/inputpwaves.VID";
     //String pathToDataset = "/home/wilsonmr/javaseis/100-rawsyntheticdata.js";
-    String pathToDataset = "/home/wilsonmr/javaseis/100a-rawsynthpwaves.js";
-    //String pathToDataset = "/home/wilsonmr/javaseis/seg_salt_vrms.VEL";
-    //String pathToDataset = "/home/wilsonmr/javaseis/941cwtmigt_pp.js";	 
+    String pathToDataset = "/home/wilsonmr/javaseis/testFFT.js";
+    //String pathToDataset = "/home/wilsonmr/javaseis/seg_salt_vrms.VEL";	 
 
     if (args.length > 0) {
       pathToDataset = args[0];
