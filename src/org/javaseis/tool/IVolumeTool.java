4ff1212e492c13adfbce650226735397737a71bb
Marcus Wilson <wilsonmr@ogeop.com>
2015-06-15 19:29:08 -0600

    Note about specification for outputVolume

--- src/org/javaseis/tool/IVolumeTool.java
+++ src/org/javaseis/tool/IVolumeTool.java
@@ -6,6 +6,9 @@ public interface IVolumeTool {
 	public void serialInit( ToolContext toolContext );
 	public void parallelInit( ToolContext toolContext );
 	public boolean processVolume( ToolContext toolContext, ISeismicVolume input, ISeismicVolume output);
+	
+	//TODO what is this supposed to do?  The StandAloneVolumeTool can output data
+	//even though this method just returns false.
 	public boolean outputVolume( ToolContext toolContext, ISeismicVolume output );
 	public void parallelFinish( ToolContext toolContext);
 	public void serialFinish( ToolContext toolContext );
