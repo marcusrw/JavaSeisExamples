ac4cf693bdebe67bb2f209b1fbdb970ca2f05cc2
Marcus Wilson <wilsonmr@ogeop.com>
2015-06-15 19:34:32 -0600

    Add some method stubs

---
+++ src/org/javaseis/examples/tool/test/JTestExtractPWaveData.java
@@ -0,0 +1,35 @@
+package org.javaseis.examples.tool.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+//TODO Simple tests to make sure the subsetting method does what you expect.
+public class JTestExtractPWaveData {
+  
+  @Before
+  public void generateTestData() {
+    
+  }
+  
+  @After
+  public void removeTestDataAndOutput() {
+    
+  }
+  
+  @Test
+  public void testSingleThread() {
+    
+  }
+  
+  @Test
+  public void testDoubleThread() {
+    
+  }
+  
+  @Test
+  public void testTripleThread() {
+    
+  }
+  
+}
---
+++ src/org/javaseis/imaging/SimpleExampleMigrator.java
@@ -0,0 +1,30 @@
+package org.javaseis.imaging;
+
+/**
+ * @author wilsonmr
+ *
+ */
+public class SimpleExampleMigrator {
+  
+  //Constructor
+  public SimpleExampleMigrator() {
+    
+  }
+  
+  public void init() {
+    
+  }
+  
+  public void forwardExtrapolate() {
+    
+  }
+  
+  public void reverseExtrapolate() {
+    
+  }
+  
+  public void applyImagingCondition() {
+    
+  }
+  
+}
