38feddb17330937db3245e81f9f421b2c5e6359a
Marcus Wilson <wilsonmr@ogeop.com>
2015-07-23 15:33:22 -0600

    TODOs and unsupported exceptions for unimplemented methods

--- src/org/javaseis/imaging/SimpleExampleMigrator.java
+++ src/org/javaseis/imaging/SimpleExampleMigrator.java
@@ -5,26 +5,36 @@ package org.javaseis.imaging;
  *
  */
 public class SimpleExampleMigrator {
-  
+
   //Constructor
   public SimpleExampleMigrator() {
-    
+    //TODO implement this biz
+    throw new UnsupportedOperationException(
+        "This method has not been implemented.");
   }
-  
+
   public void init() {
-    
+    //TODO implement this biz
+    throw new UnsupportedOperationException(
+        "This method has not been implemented.");
   }
-  
+
   public void forwardExtrapolate() {
-    
+    //TODO implement this biz
+    throw new UnsupportedOperationException(
+        "This method has not been implemented.");
   }
-  
+
   public void reverseExtrapolate() {
-    
+    //TODO implement this biz
+    throw new UnsupportedOperationException(
+        "This method has not been implemented.");
   }
-  
+
   public void applyImagingCondition() {
-    
+    //TODO implement this biz
+    throw new UnsupportedOperationException(
+        "This method has not been implemented.");
   }
-  
+
 }
