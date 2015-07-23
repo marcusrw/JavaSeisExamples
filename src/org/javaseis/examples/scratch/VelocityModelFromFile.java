11d5484839faf50cb7b857c06b964f4445e7ed47
Marcus Wilson <wilsonmr@ogeop.com>
2015-07-22 18:49:23 -0600

    Initial skeleton of readvelocityfromfile object

---
+++ src/org/javaseis/examples/scratch/VelocityModelFromFile.java
@@ -0,0 +1,21 @@
+package org.javaseis.examples.scratch;
+
+public class VelocityModelFromFile {
+  
+  //Start a seisio handle to read velocity slices in from a file.
+  
+  public VelocityModelFromFile(String filepath) {
+    
+  }
+  
+  public void open(String openMode) {
+    //open the seisio and possibly other stuff
+  }
+  
+  public void close() {
+    //close the seisio and possibly other stuff
+  }
+  
+  
+
+}
