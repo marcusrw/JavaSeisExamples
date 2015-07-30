065ea1d64b437a5ac11f154072a667f9fb316c9e
Marcus Wilson <wilsonmr@ogeop.com>
2015-07-30 17:05:54 -0600

    Added very rough extrapolator and imaging condition

---
+++ src/org/javaseis/examples/scratch/Extrapolator.java
@@ -0,0 +1,95 @@
+package org.javaseis.examples.scratch;
+
+import java.util.logging.Logger;
+
+import beta.javaseis.distributed.DistributedArray;
+import beta.javaseis.distributed.DistributedArrayPositionIterator;
+
+import org.javaseis.util.IntervalTimer;
+
+public class Extrapolator {
+
+  private static final Logger LOGGER = 
+      Logger.getLogger(Extrapolator.class.getName());
+
+  SeisFft3dNew shot;
+  SeisFft3dNew rcvr;
+
+  IntervalTimer extrapTime;
+
+  public Extrapolator(SeisFft3dNew shot,SeisFft3dNew rcvr) {
+    this.rcvr = rcvr;
+    this.shot = shot;
+    extrapTime = new IntervalTimer();
+  }
+
+  //Constant velocity phase shift
+  public void extrapolate(float velocity, double delz,int zindx,double fMax) {
+
+    extrapTime.start();
+
+    float eps = 1E-12F;
+    eps = 0F;
+
+    DistributedArray rcvrDA = rcvr.getArray();
+    DistributedArray shotDA = shot.getArray();
+
+    int[] position = new int[rcvrDA.getDimensions()];
+    int direction = 1; //forward
+    int scope = 0; //samples
+
+    //buffers
+    float[] recInSample = new float[rcvrDA.getElementCount()];
+    float[] souInSample = new float[shotDA.getElementCount()];
+    float[] recOutSample = new float[rcvrDA.getElementCount()];
+    float[] souOutSample = new float[rcvrDA.getElementCount()];
+    double[] coords = new double[position.length];
+
+    DistributedArrayPositionIterator dapi =
+        new DistributedArrayPositionIterator(
+            rcvrDA,position,direction,scope);
+    while (dapi.hasNext()) {
+      position = dapi.next();
+      rcvrDA.getSample(recInSample, position);
+      shotDA.getSample(souInSample, position);
+      rcvr.getKyKxFCoordinatesForPosition(position, coords);
+      double kY = coords[0];
+      double kX = coords[1];
+      double frequency = coords[2];
+
+      //skip if we're over the data threshold.
+      //TODO put this back when you have a proper band limited source.
+      //if (frequency > fMax) continue;
+
+      double Kz2 = (frequency/velocity)*(frequency/velocity) - kY*kY - kX*kX;
+      double exponent = 0;
+      if (Kz2 > eps && zindx > 0) {
+        exponent = (-2*Math.PI*delz * Math.sqrt(Kz2));
+      }
+      if (Kz2 > eps) {
+        //TODO fix these if statements so we get proper filtering
+        // for depth z = 0.
+
+        //TODO use complex math methods instead of doing it manually
+        recOutSample[0] = (float) (recInSample[0]*Math.cos(-exponent)
+            - recInSample[1]*Math.sin(-exponent));
+        recOutSample[1] = (float) (recInSample[1]*Math.cos(-exponent)
+            + recInSample[0]*Math.sin(-exponent));
+        souOutSample[0] = (float) (souInSample[0]*Math.cos(exponent)
+            - souInSample[1]*Math.sin(exponent));
+        souOutSample[1] = (float) (souInSample[1]*Math.cos(exponent)
+            + souInSample[0]*Math.sin(exponent));
+
+      } else {
+        exponent = 2*Math.PI*Math.abs(delz)*Math.sqrt(Math.abs(Kz2));
+        recOutSample[0] = (float) (recInSample[0]*Math.exp(-exponent));
+        recOutSample[1] = (float) (recInSample[1]*Math.exp(-exponent));
+        souOutSample[0] = (float) (souInSample[0]*Math.exp(-exponent));
+        souOutSample[1] = (float) (souInSample[1]*Math.exp(-exponent));
+      }
+      rcvrDA.putSample(recOutSample, position);
+      shotDA.putSample(souOutSample, position);
+    }
+    extrapTime.stop();
+  }
+}
---
+++ src/org/javaseis/examples/scratch/ImagingCondition.java
@@ -0,0 +1,102 @@
+package org.javaseis.examples.scratch;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+import org.javaseis.util.IntervalTimer;
+
+import beta.javaseis.distributed.DistributedArray;
+import beta.javaseis.distributed.DistributedArrayPositionIterator;
+
+public class ImagingCondition {
+  
+  private static final Logger LOGGER = 
+      Logger.getLogger(ImagingCondition.class.getName());
+  
+  SeisFft3dNew shot;
+  SeisFft3dNew rcvr;
+  
+  DistributedArray imageDA;
+  
+  IntervalTimer imagingTime;
+  
+  public ImagingCondition(SeisFft3dNew shot,SeisFft3dNew rcvr,
+      DistributedArray imageDA) {
+    this.shot = shot;
+    this.rcvr = rcvr;
+    this.imageDA = imageDA;
+    imagingTime = new IntervalTimer();
+  }
+  
+  public void imagingCondition(DistributedArray imageDA,
+      int zindx,double fMax) {
+    imagingTime.start();
+
+    DistributedArray rcvrDA = rcvr.getArray();
+    DistributedArray shotDA = shot.getArray();
+    float[] recInSample2 = new float[rcvrDA.getElementCount()];
+    float[] souInSample2 = new float[shotDA.getElementCount()];
+
+    //TODO Trick.  Hide the high frequencies from the iterator
+    // so that it doesn't waste time accumulating a bunch of zeros.
+
+
+    int[] fullShape = rcvrDA.getShape().clone();
+    LOGGER.info(Arrays.toString(fullShape));
+
+    /*
+    double fNY = 1/(2*0.002);
+    double delf = fNY/DALengths[0];
+    int realMaxF = DALengths[0];
+    int maxFindx = (int) (fMax/delf)+1;
+
+    //DALengths[0] = maxFindx;
+    LOGGER.info("Max F index: " + maxFindx);
+    rcvrDA.setShape(DALengths);
+    shotDA.setShape(DALengths);
+    LOGGER.info(Arrays.toString(DALengths));
+     */
+
+
+
+    //buffers
+    int[] position = new int[rcvrDA.getDimensions()];
+    int direction = 1; //forward
+    int scope = 0; //samples
+
+    DistributedArrayPositionIterator dapi;
+    dapi = new DistributedArrayPositionIterator(rcvrDA,position,
+        direction,scope);
+
+    float[] imageSample = new float[imageDA.getElementCount()];
+    while (dapi.hasNext()) {
+      position = dapi.next();
+      int[] outputPosition = position.clone();
+      outputPosition[0] = zindx;
+      rcvrDA.getSample(recInSample2, position);
+      shotDA.getSample(souInSample2, position);
+
+      imageDA.getSample(imageSample, outputPosition);
+      imageSample[0] += recInSample2[0]*souInSample2[0]
+          + recInSample2[1]*souInSample2[1];
+      imageDA.putSample(imageSample, outputPosition);
+    }
+
+    //Get the source and receiver samples
+    LOGGER.info("\n\nShot DA shape: "
+        + Arrays.toString(shotDA.getShape())
+        + "\nShot DA sample count: "
+        + shotDA.getTotalSampleCount()
+        +"\n\nReceiver DA shape: "
+        + Arrays.toString(rcvrDA.getShape()) 
+        + "\nReceiver DA sample count: "
+        + rcvr.getArray().getTotalSampleCount()
+        +"\n\nImage DA shape: " 
+        + Arrays.toString(imageDA.getShape()) 
+        + "\nImage DA sample count: " 
+        + imageDA.getTotalSampleCount()
+        + "\n\n");
+
+    imagingTime.stop();
+  }
+}
