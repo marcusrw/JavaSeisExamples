de8db9e28c6e6a0b6d612849b18f1c9a72dda3ff
Marcus Wilson <wilsonmr@ogeop.com>
2015-08-04 15:27:59 -0600

    indents.  setup for green source

--- src/org/javaseis/examples/scratch/SourceVolume.java
+++ src/org/javaseis/examples/scratch/SourceVolume.java
@@ -4,138 +4,137 @@ import beta.javaseis.distributed.DistributedArray;
 
 public class SourceVolume implements ISourceVolume {
 
-	SeisFft3dNew shot;
-	double[] physicalSourceXYZ;
-	float[] arraySourceXYZ;
-	int[] AXIS_ORDER;
+  SeisFft3dNew shot;
+  double[] physicalSourceXYZ;
+  float[] arraySourceXYZ;
+  int[] AXIS_ORDER;
 
-	public SourceVolume(ICheckGrids CheckedGrid, SeisFft3dNew shot) {
-		// Get the physical source
-		this.physicalSourceXYZ = CheckedGrid.getSourceXYZ();
+  public SourceVolume(ICheckGrids CheckedGrid, SeisFft3dNew shot) {
+    // Get the physical source
+    this.physicalSourceXYZ = CheckedGrid.getSourceXYZ();
 
-		// Get the Axis Order
-		this.AXIS_ORDER = CheckedGrid.getAxisOrder();
+    // Get the Axis Order
+    this.AXIS_ORDER = CheckedGrid.getAxisOrder();
 
-		// Compute the Array coordinates based on the grid
-		this.arraySourceXYZ = covertPhysToArray(this.physicalSourceXYZ, CheckedGrid);
+    // Compute the Array coordinates based on the grid
+    this.arraySourceXYZ = covertPhysToArray(this.physicalSourceXYZ, CheckedGrid);
 
-		// Check the shot
-		this.shot = CheckShot(shot);
+    // Check the shot DA is in the right domain (FXY in this case)
+    this.shot = checkShotIsInFXY(shot);
 
-		// Generate
-		generateSourceSignature(arraySourceXYZ);
+    // Generate
+    generateSourceSignature(arraySourceXYZ);
 
-	}
+  }
 
-	public SourceVolume(ICheckGrids CheckedGrid, SeisFft3dNew shot, double[] physicalSourceXYZ, int[] AXIS_ORDER) {
-		// Get the physical source
-		this.physicalSourceXYZ = physicalSourceXYZ;
+  public SourceVolume(ICheckGrids CheckedGrid, SeisFft3dNew shot, double[] physicalSourceXYZ, int[] AXIS_ORDER) {
+    // Get the physical source
+    this.physicalSourceXYZ = physicalSourceXYZ;
 
-		// Get the Axis Order
-		this.AXIS_ORDER = AXIS_ORDER;
+    // Get the Axis Order
+    this.AXIS_ORDER = AXIS_ORDER;
 
-		// Need to convert this grid into array coordinates
-		this.arraySourceXYZ = covertPhysToArray(this.physicalSourceXYZ, CheckedGrid);
+    // Need to convert this grid into array coordinates
+    this.arraySourceXYZ = covertPhysToArray(this.physicalSourceXYZ, CheckedGrid);
 
-		this.shot = CheckShot(shot);
+    this.shot = checkShotIsInFXY(shot);
 
-		// Generate
-		generateSourceSignature(arraySourceXYZ);
+    // Generate
+    generateSourceSignature(arraySourceXYZ);
 
-	}
+  }
 
-	private SeisFft3dNew CheckShot(SeisFft3dNew shot) {
-		if (!shot.isTimeTransformed()) {
-			shot.forwardTemporal();
-			return shot;
-		}
-		return shot;
-	}
+  private SeisFft3dNew checkShotIsInFXY(SeisFft3dNew shot) {
+    if (!shot.isTimeTransformed()) {
+      shot.forwardTemporal();
+      return shot;
+    }
+    return shot;
+  }
 
-	/*
-	 * Converts the physical coordinates to Array Coordinates
-	 */
-	public float[] covertPhysToArray(double[] sourceXYZ, ICheckGrids CheckedGrid) {
-		//Correct
-		//System.out.println("[covertPhysToArray]: " + Arrays.toString(sourceXYZ));
-		
-		// output buffer
-		float[] vS = new float[AXIS_ORDER.length];
+  /*
+   * Converts the physical coordinates to Array Coordinates
+   */
+  public float[] covertPhysToArray(double[] sourceXYZ, ICheckGrids CheckedGrid) {
+    //Correct
+    //System.out.println("[covertPhysToArray]: " + Arrays.toString(sourceXYZ));
 
-		for (int i = 0; i < AXIS_ORDER.length; i++) {
-			int currentAxis = CheckedGrid.getAxisOrder()[i];
-			double minPhys0 = CheckedGrid.getModifiedGrid().getAxisPhysicalOrigin(currentAxis);
-			double axisPhysDelta = CheckedGrid.getModifiedGrid().getAxisPhysicalDelta(currentAxis);
-			vS[i] = (float) ((sourceXYZ[i] - minPhys0) / axisPhysDelta);
-		}
-		return vS;
-	}
+    // output buffer
+    float[] vS = new float[AXIS_ORDER.length];
 
-	private void generateSourceSignature(float[] sourceXYZ) {
-		//System.out.println("[generateSourceSignature]: " + Arrays.toString(sourceXYZ));
-		if (sourceXYZ.length != 3)
-			throw new IllegalArgumentException("Wrong number of elements for sourceXYZ");
-		
-		//TODO: !!!!!THIS IS NOT CORRECT FIX IT AT SOME POINT!!!!!
-		if (sourceXYZ[2] > 0){
-			sourceXYZ[2] = 0;
-			System.out.println("Source Depth changed to: " + sourceXYZ[2]);
-			//throw new UnsupportedOperationException("Sources at Depths besides zero not yet implemented");
-		}
+    for (int i = 0; i < AXIS_ORDER.length; i++) {
+      int currentAxis = CheckedGrid.getAxisOrder()[i];
+      double minPhys0 = CheckedGrid.getModifiedGrid().getAxisPhysicalOrigin(currentAxis);
+      double axisPhysDelta = CheckedGrid.getModifiedGrid().getAxisPhysicalDelta(currentAxis);
+      vS[i] = (float) ((sourceXYZ[i] - minPhys0) / axisPhysDelta);
+    }
+    return vS;
+  }
 
-		int sourceX = (int) Math.floor(sourceXYZ[0]);
-		while (sourceX < sourceXYZ[0] + 1) {
-			int sourceY = (int) Math.floor(sourceXYZ[1]);
-			while (sourceY < sourceXYZ[1] + 1) {
-				float weight = Math.max(0, 1 - euclideanDistance(sourceX, sourceY, sourceXYZ));
-				putWhiteSpectrum(shot, sourceX, sourceY, weight);
-				sourceY++;
-			}
-			sourceX++;
-		}
-	}
+  private void generateSourceSignature(float[] sourceXYZ) {
+    //System.out.println("[generateSourceSignature]: " + Arrays.toString(sourceXYZ));
+    if (sourceXYZ.length != 3)
+      throw new IllegalArgumentException("Wrong number of elements for sourceXYZ");
 
-	/**
-	 * @param sourceX
-	 *            - Target X index in the grid
-	 * @param sourceY
-	 *            - Target Y index in the grid
-	 * @param sourceXYZ
-	 *            - Actual Source position in the grid
-	 * @return The Euclidean distance between the current array index and the
-	 *         input source.
-	 */
-	private float euclideanDistance(float sourceX, float sourceY, float[] sourceXYZ) {
+    //TODO: !!!!!THIS IS NOT CORRECT FIX IT AT SOME POINT!!!!!
+    if (sourceXYZ[2] > 0){
+      sourceXYZ[2] = 0;
+      System.out.println("Source Depth changed to: " + sourceXYZ[2]);
+      //throw new UnsupportedOperationException("Sources at Depths besides zero not yet implemented");
+    }
 
-		float dx2 = (sourceX - sourceXYZ[0]) * (sourceX - sourceXYZ[0]);
-		float dy2 = (sourceY - sourceXYZ[1]) * (sourceY - sourceXYZ[1]);
-		return (float) Math.sqrt(dx2 + dy2);
-	}
+    int sourceX = (int) Math.floor(sourceXYZ[0]);
+    while (sourceX < sourceXYZ[0] + 1) {
+      int sourceY = (int) Math.floor(sourceXYZ[1]);
+      while (sourceY < sourceXYZ[1] + 1) {
+        float weight = Math.max(0, 1 - euclideanDistance(sourceX, sourceY, sourceXYZ));
+        putWhiteSpectrum(shot, sourceX, sourceY, weight);
+        sourceY++;
+      }
+      sourceX++;
+    }
+  }
 
-	private void putWhiteSpectrum(SeisFft3dNew source, int sourceX, int sourceY, float amplitude) {
+  /**
+   * @param sourceX
+   *            - Target X index in the grid
+   * @param sourceY
+   *            - Target Y index in the grid
+   * @param sourceXYZ
+   *            - Actual Source position in the grid
+   * @return The Euclidean distance between the current array index and the
+   *         input source.
+   */
+  private float euclideanDistance(float sourceX, float sourceY, float[] sourceXYZ) {
 
-		int[] position = new int[] { 0, sourceX, sourceY };
-		int[] volumeShape = source.getArray().getShape();
-		float[] sample = new float[] { amplitude, 0 }; // amplitude+0i complex
-		DistributedArray sourceDA = source.getArray();
-		while (position[0] < volumeShape[0]) {
-			sourceDA.putSample(sample, position);
-			position[0]++;
-		}
-	}
+    float dx2 = (sourceX - sourceXYZ[0]) * (sourceX - sourceXYZ[0]);
+    float dy2 = (sourceY - sourceXYZ[1]) * (sourceY - sourceXYZ[1]);
+    return (float) Math.sqrt(dx2 + dy2);
+  }
 
-	/*
-	 * Returns the SeisFft3dNew Object (shot)
-	 */
-	public SeisFft3dNew getShot() {
-		return shot;
-	}
+  private void putWhiteSpectrum(SeisFft3dNew source, int sourceX, int sourceY, float amplitude) {
 
-	/*
-	 * Returns the DistributedArray of the shot
-	 */
-	public DistributedArray getDistributedArray() {
-		return shot.getArray();
-	}
+    int[] position = new int[] { 0, sourceX, sourceY };
+    int[] volumeShape = source.getArray().getShape();
+    float[] sample = new float[] { amplitude, 0 }; // amplitude+0i complex
+    DistributedArray sourceDA = source.getArray();
+    while (position[0] < volumeShape[0]) {
+      sourceDA.putSample(sample, position);
+      position[0]++;
+    }
+  }
 
+  /*
+   * Returns the SeisFft3dNew Object (shot)
+   */
+  public SeisFft3dNew getShot() {
+    return shot;
+  }
+
+  /*
+   * Returns the DistributedArray of the shot
+   */
+  public DistributedArray getDistributedArray() {
+    return shot.getArray();
+  }
 }
