1b9a2aa5984bc8ce1b657a681af100b56b6be70a
Marcus Wilson <wilsonmr@ogeop.com>
2015-08-05 16:42:18 -0600

    Change output layout of VolumeEdge summary.  Moved velocity model to the top and removed repetition, and added some new fields and whitespace to the output for each volume.

--- src/org/javaseis/examples/scratch/VolumeEdgeIO.java
+++ src/org/javaseis/examples/scratch/VolumeEdgeIO.java
@@ -21,258 +21,277 @@ import beta.javaseis.parallel.IParallelContext;
 
 public class VolumeEdgeIO {
 
-	private static int volumeNumber = 0; // Start at 0 volume
-	private ToolContext toolContext;
-	private String originalPN;
-	private IDistributedIOService ipio = null;
-	private VelocityModelFromFile vff = null;
-	private ISeismicVolume seismicInput;
-	private ICheckGrids checkGrid;
-	private PrintWriter out = null;;
+  private static int volumeNumber = 0; // Start at 0 volume
+  private ToolContext toolContext;
+  private String originalPN;
+  private IDistributedIOService ipio = null;
+  private VelocityModelFromFile vff = null;
+  private ISeismicVolume seismicInput;
+  private ICheckGrids checkGrid;
+  private PrintWriter out = null;;
 
-	public VolumeEdgeIO(IParallelContext pc, ToolContext toolContext) {
+  public VolumeEdgeIO(IParallelContext pc, ToolContext toolContext) {
 
-		try {
-			ipio = new FileSystemIOService(pc, toolContext.getParameter(ToolContext.INPUT_FILE_SYSTEM));
-		} catch (SeisException e) {
-			e.printStackTrace();
-		}
+    try {
+      ipio = new FileSystemIOService(pc, toolContext.getParameter(ToolContext.INPUT_FILE_SYSTEM));
+    } catch (SeisException e) {
+      e.printStackTrace();
+    }
 
-		// Open your chosen file for reading
-		try {
-			ipio.open(toolContext.getParameter(ToolContext.INPUT_FILE_PATH));
-		} catch (SeisException e) {
-			e.printStackTrace();
-		}
+    // Open your chosen file for reading
+    try {
+      ipio.open(toolContext.getParameter(ToolContext.INPUT_FILE_PATH));
+    } catch (SeisException e) {
+      e.printStackTrace();
+    }
 
-		originalPN = toolContext.getParameter(ToolContext.OUTPUT_FILE_SYSTEM)
-				+ "//"
-				+ toolContext.getParameter(ToolContext.OUTPUT_FILE_PATH)
-				+ "//vEdge";
-		
-		//System.out.println(originalPN);
-		
-		// Get the global grid definition
-		GridDefinition globalGrid = ipio.getGridDefinition();
+    originalPN = toolContext.getParameter(ToolContext.OUTPUT_FILE_SYSTEM)
+        + "//"
+        + toolContext.getParameter(ToolContext.OUTPUT_FILE_PATH)
+        + "//vEdge";
 
-		// Now you have enough to make a SeismicVolume
-		ISeismicVolume inputVolume = new SeismicVolume(pc, globalGrid);
-		this.seismicInput = inputVolume;
+    //System.out.println(originalPN);
 
-		// match the IO's DA with the Volume's DA
-		ipio.setDistributedArray(inputVolume.getDistributedArray());
+    // Get the global grid definition
+    GridDefinition globalGrid = ipio.getGridDefinition();
 
-		// Get the Input and tool context
-		this.toolContext = toolContext;
+    // Now you have enough to make a SeismicVolume
+    ISeismicVolume inputVolume = new SeismicVolume(pc, globalGrid);
+    this.seismicInput = inputVolume;
 
-	}
+    // match the IO's DA with the Volume's DA
+    ipio.setDistributedArray(inputVolume.getDistributedArray());
 
-	// Generate the file path for each volume
-	private String generateNextPath() {
-		// take the original file path and set it to the new one
-		String newPN = originalPN;
+    // Get the Input and tool context
+    this.toolContext = toolContext;
 
-		// the VolNum
-		newPN += volumeNumber;
+  }
 
-		// Inc the global volume number
-		volumeNumber++;
+  // Generate the file path for each volume
+  private String generateNextPath() {
+    // take the original file path and set it to the new one
+    String newPN = originalPN;
 
-		// Make it a txt file
-		newPN += ".txt";
+    // the VolNum
+    newPN += volumeNumber;
 
-		// Expected string form [generateNextPath(0) = //tmp//vEdge0.txt]
-		return newPN;
-	}
+    // Inc the global volume number
+    volumeNumber++;
 
-	// Write the grid data to output specific file
-	private void writeGridInformation(PrintWriter out) {
-		// Iterate through the DistArray get the volumes
-		DistributedArray curArray = seismicInput.getDistributedArray();
+    // Make it a txt file
+    newPN += ".txt";
 
-		// get the start of the current volume
-		int[] globalPosIndex = seismicInput.getVolumePosition();
-		int[] volumePosIndex = new int[3];
+    // Expected string form [generateNextPath(0) = //tmp//vEdge0.txt]
+    return newPN;
+  }
 
-		// Iterate over the Volume - Scope = 3
-		DistributedArrayPositionIterator itrInputArr = new DistributedArrayPositionIterator(curArray, volumePosIndex,
-				DistributedArrayPositionIterator.FORWARD, 3);
+  // Write the grid data to output specific file
+  private void writeGridInformation(PrintWriter out) {
+    // Iterate through the DistArray get the volumes
+    DistributedArray curArray = seismicInput.getDistributedArray();
 
-		// source and receiver arrays
-		double[] rXYZ = new double[3];
+    // get the start of the current volume
+    int[] globalPosIndex = seismicInput.getVolumePosition();
+    int[] volumePosIndex = new int[3];
 
-		// Start location of volume
-		out.println("[getVolumeEdges]: Volume Source Location: " + Arrays.toString(checkGrid.getSourceXYZ()));
+    // Iterate over the Volume - Scope = 3
+    DistributedArrayPositionIterator itrInputArr 
+    = new DistributedArrayPositionIterator(curArray, volumePosIndex,
+        DistributedArrayPositionIterator.FORWARD, 3);
 
-		while (itrInputArr.hasNext()) {
-			globalPosIndex[0] = 0;
-			volumePosIndex = itrInputArr.next();
-			for (int k = 1; k < 3; k++) {
-				globalPosIndex[k] = volumePosIndex[k];
-			}
+    // source and receiver arrays
+    double[] rXYZ = new double[3];
 
-			// Get the receiver & source at [0, axislength - 1, axislength - 1,
-			// v]
-			long X = seismicInput.getGlobalGrid().getAxisLength(checkGrid.getAxisOrder()[0]) - 1;
-			long Y = seismicInput.getGlobalGrid().getAxisLength(checkGrid.getAxisOrder()[1]) - 1;
+    out.println("[getVolumeEdges]: Volume Index: "
+        + Arrays.toString(globalPosIndex));
 
-			// Example x = 0, y = 0;
-			globalPosIndex[1] = 0;
-			globalPosIndex[2] = 0;
+    out.println("[getVolumeEdges]: Axis Order: "
+        + Arrays.toString(checkGrid.getAxisOrder()));
 
-			rXYZ = checkGrid.getReceiverXYZ(globalPosIndex);
+    // Start location of volume
+    out.println("[getVolumeEdges]: Volume Source Location: "
+        + Arrays.toString(checkGrid.getSourceXYZ()));
 
-			out.println("[getVolumeEdges]: [s,t,f,v]: " + Arrays.toString(globalPosIndex) + " \tReceiverXYZ: "
-					+ Arrays.toString(rXYZ));
+    while (itrInputArr.hasNext()) {
+      globalPosIndex[0] = 0;
+      volumePosIndex = itrInputArr.next();
+      for (int k = 1; k < 3; k++) {
+        globalPosIndex[k] = volumePosIndex[k];
+      }
 
-			// Example x = 200, y = 0
-			globalPosIndex[1] = (int) X;
-			globalPosIndex[2] = 0;
+      // Get the receiver & source at [0, axislength - 1, axislength - 1,
+      // v]
+      long X = seismicInput.getGlobalGrid().getAxisLength(checkGrid.getAxisOrder()[0]) - 1;
+      long Y = seismicInput.getGlobalGrid().getAxisLength(checkGrid.getAxisOrder()[1]) - 1;
 
-			rXYZ = checkGrid.getReceiverXYZ(globalPosIndex);
+      // Example x = 0, y = 0;
+      globalPosIndex[1] = 0;
+      globalPosIndex[2] = 0;
 
-			// Get bottom right coords
-			out.println("[getVolumeEdges]: [s,t,f,v]: " + Arrays.toString(globalPosIndex) + " \tReceiverXYZ: "
-					+ Arrays.toString(rXYZ));
+      rXYZ = checkGrid.getReceiverXYZ(globalPosIndex);
 
-			// Get top left
-			// Example x = 0, y = 200
-			globalPosIndex[1] = 0;
-			globalPosIndex[2] = (int) Y;
+      out.println("[getVolumeEdges]: [s,t,f,v]: " + Arrays.toString(globalPosIndex) + " \tReceiverXYZ: "
+          + Arrays.toString(rXYZ));
 
-			rXYZ = checkGrid.getReceiverXYZ(globalPosIndex);
-			out.println("[getVolumeEdges]: [s,t,f,v]: " + Arrays.toString(globalPosIndex) + " \tReceiverXYZ: "
-					+ Arrays.toString(rXYZ));
+      // Example x = 200, y = 0
+      globalPosIndex[1] = (int) X;
+      globalPosIndex[2] = 0;
 
-			// Get top right
-			// Example x = 200, y = 200
-			globalPosIndex[1] = (int) X;
-			globalPosIndex[2] = (int) Y;
+      rXYZ = checkGrid.getReceiverXYZ(globalPosIndex);
 
-			rXYZ = checkGrid.getReceiverXYZ(globalPosIndex);
-			out.println("[getVolumeEdges]: [s,t,f,v]: " + Arrays.toString(globalPosIndex) + " \tReceiverXYZ: "
-					+ Arrays.toString(rXYZ));
+      // Get bottom right coords
+      out.println("[getVolumeEdges]: [s,t,f,v]: " 
+          + Arrays.toString(globalPosIndex) + " \tReceiverXYZ: "
+          + Arrays.toString(rXYZ));
 
-		}
+      // Get top left
+      // Example x = 0, y = 200
+      globalPosIndex[1] = 0;
+      globalPosIndex[2] = (int) Y;
 
-	}
+      rXYZ = checkGrid.getReceiverXYZ(globalPosIndex);
+      out.println("[getVolumeEdges]: [s,t,f,v]: " 
+          + Arrays.toString(globalPosIndex) + " \tReceiverXYZ: "
+          + Arrays.toString(rXYZ));
 
-	// Write the Velo data to output specific file
-	private void writeVelocityInformation(PrintWriter out) {
-		GridDefinition vmodelGrid = vff.vmodelGrid;
-		// System.out.println("[VelocityInformation:] " +
-		// vmodelGrid.toString());
-		// System.out.println("[VelocityInformation:] " +
-		// vmodelGrid.getNumDimensions());
+      // Get top right
+      // Example x = 200, y = 200
+      globalPosIndex[1] = (int) X;
+      globalPosIndex[2] = (int) Y;
 
-		int[] veloPos = new int[3];
-		double[] veloValue = new double[3];
+      rXYZ = checkGrid.getReceiverXYZ(globalPosIndex);
+      out.println("[getVolumeEdges]: [s,t,f,v]: " 
+          + Arrays.toString(globalPosIndex) + " \tReceiverXYZ: "
+          + Arrays.toString(rXYZ));
 
-		int XAxis = 2;
-		int YAxis = 1;
-		int ZAxis = 0;
+    }
 
-		long lengthX = vmodelGrid.getAxisLength(XAxis);
-		long lengthY = vmodelGrid.getAxisLength(YAxis);
+  }
 
-		veloPos[0] = 0; // velocity time axis
-		veloPos[1] = 0; // velocity cross axis - y
-		veloPos[2] = 0; // velocity inline axis - x
+  // Write the Velo data to output specific file
+  private void writeVelocityInformation(PrintWriter out) {
+    GridDefinition vmodelGrid = vff.vmodelGrid;
+    // System.out.println("[VelocityInformation:] " +
+    // vmodelGrid.toString());
+    // System.out.println("[VelocityInformation:] " +
+    // vmodelGrid.getNumDimensions());
 
-		// Velo at [0,0,0]
-		veloValue[0] = vmodelGrid.getAxisPhysicalOrigin(XAxis) + veloPos[2] * vmodelGrid.getAxisPhysicalDelta(XAxis);
-		veloValue[1] = vmodelGrid.getAxisPhysicalOrigin(YAxis) + veloPos[1] * vmodelGrid.getAxisPhysicalDelta(YAxis);
-		veloValue[2] = vmodelGrid.getAxisPhysicalOrigin(ZAxis) + veloPos[0] * vmodelGrid.getAxisPhysicalDelta(ZAxis);
-		
-		out.println("[VelocityInformation]: " + "[s,t,f]: " + Arrays.toString(veloPos) + " \t[x, y, d]: "
-				+ Arrays.toString(veloValue));
+    int[] veloPos = new int[3];
+    double[] veloValue = new double[3];
 
-		// Velo at [0, 675, 0]
-		veloPos[1] = (int) (lengthY - 1); // velocity cross axis - y
-		veloPos[2] = 0; // velocity inline axis - x
-		veloValue[0] = vmodelGrid.getAxisPhysicalOrigin(XAxis) + veloPos[2] * vmodelGrid.getAxisPhysicalDelta(XAxis);
-		veloValue[1] = vmodelGrid.getAxisPhysicalOrigin(YAxis) + veloPos[1] * vmodelGrid.getAxisPhysicalDelta(YAxis);
-		veloValue[2] = vmodelGrid.getAxisPhysicalOrigin(ZAxis) + veloPos[0] * vmodelGrid.getAxisPhysicalDelta(ZAxis);
-		
-		out.println("[VelocityInformation]: " + "[s,t,f]: " + Arrays.toString(veloPos) + " \t[x, y, d]: "
-				+ Arrays.toString(veloValue));
+    int XAxis = 2;
+    int YAxis = 1;
+    int ZAxis = 0;
 
-		// Velo at [0, 0, 675]
-		veloPos[1] = 0; // velocity cross axis - y
-		veloPos[2] = (int) (lengthX - 1); // velocity inline axis - x
-		veloValue[0] = vmodelGrid.getAxisPhysicalOrigin(XAxis) + veloPos[2] * vmodelGrid.getAxisPhysicalDelta(XAxis);
-		veloValue[1] = vmodelGrid.getAxisPhysicalOrigin(YAxis) + veloPos[1] * vmodelGrid.getAxisPhysicalDelta(YAxis);
-		veloValue[2] = vmodelGrid.getAxisPhysicalOrigin(ZAxis) + veloPos[0] * vmodelGrid.getAxisPhysicalDelta(ZAxis);
-		
-		out.println("[VelocityInformation]: " + "[s,t,f]: " + Arrays.toString(veloPos) + " \t[x, y, d]: "
-				+ Arrays.toString(veloValue));
+    long lengthX = vmodelGrid.getAxisLength(XAxis);
+    long lengthY = vmodelGrid.getAxisLength(YAxis);
 
-		// Velo at [0, 675, 675]
-		veloPos[1] = (int) (lengthY - 1); // velocity cross axis - y
-		veloPos[2] = (int) (lengthX - 1); // velocity inline axis - x
-		veloValue[0] = vmodelGrid.getAxisPhysicalOrigin(XAxis) + veloPos[2] * vmodelGrid.getAxisPhysicalDelta(XAxis);
-		veloValue[1] = vmodelGrid.getAxisPhysicalOrigin(YAxis) + veloPos[1] * vmodelGrid.getAxisPhysicalDelta(YAxis);
-		veloValue[2] = vmodelGrid.getAxisPhysicalOrigin(ZAxis) + veloPos[0] * vmodelGrid.getAxisPhysicalDelta(ZAxis);
-		
-		out.println("[VelocityInformation]: " + "[s,t,f]: " + Arrays.toString(veloPos) + " \t[x, y, d]: "
-				+ Arrays.toString(veloValue));
+    veloPos[0] = 0; // velocity time axis
+    veloPos[1] = 0; // velocity cross axis - y
+    veloPos[2] = 0; // velocity inline axis - x
 
-	}
-	
-	private void initalizeWriter(){
-		File f = new File(originalPN);
-	    if (f.exists() && !f.isDirectory()) {
-	      f.delete();
-	    }
-		try {
-			FileWriter fWrtr = new FileWriter(generateNextPath());
-			out = new PrintWriter(fWrtr);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	private void closeWriter(){
-		if (out != null) {
-		      out.close();
-		}
-	}
+    out.println("Velocity Grid Information");
 
-	// Write to external file
-	public void write() {
-		initalizeWriter();
-		while (ipio.hasNext()) {
-			ipio.next();
-			seismicInput.setVolumePosition(ipio.getFilePosition());
+    // Velo at [0,0,0]
+    veloValue[0] = vmodelGrid.getAxisPhysicalOrigin(XAxis) + veloPos[2] * vmodelGrid.getAxisPhysicalDelta(XAxis);
+    veloValue[1] = vmodelGrid.getAxisPhysicalOrigin(YAxis) + veloPos[1] * vmodelGrid.getAxisPhysicalDelta(YAxis);
+    veloValue[2] = vmodelGrid.getAxisPhysicalOrigin(ZAxis) + veloPos[0] * vmodelGrid.getAxisPhysicalDelta(ZAxis);
 
-			try {
-				ipio.read();
-			} catch (SeisException e) {
-				e.printStackTrace();
-			}
+    out.println("[VelocityInformation]: " + "[s,t,f]: " + Arrays.toString(veloPos) + " \t[x, y, d]: "
+        + Arrays.toString(veloValue));
 
-			checkGrid = new CheckGrids(seismicInput, toolContext);
+    // Velo at [0, 675, 0]
+    veloPos[1] = (int) (lengthY - 1); // velocity cross axis - y
+    veloPos[2] = 0; // velocity inline axis - x
+    veloValue[0] = vmodelGrid.getAxisPhysicalOrigin(XAxis) + veloPos[2] * vmodelGrid.getAxisPhysicalDelta(XAxis);
+    veloValue[1] = vmodelGrid.getAxisPhysicalOrigin(YAxis) + veloPos[1] * vmodelGrid.getAxisPhysicalDelta(YAxis);
+    veloValue[2] = vmodelGrid.getAxisPhysicalOrigin(ZAxis) + veloPos[0] * vmodelGrid.getAxisPhysicalDelta(ZAxis);
 
-			try {
-				vff = new VelocityModelFromFile(toolContext);
-				vff.open("r");
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			// Write the Volume Information to the file
-			writeVelocityInformation(out);
+    out.println("[VelocityInformation]: " + "[s,t,f]: " + Arrays.toString(veloPos) + " \t[x, y, d]: "
+        + Arrays.toString(veloValue));
 
-			// Write the Grid Information to the file
-			writeGridInformation(out);
+    // Velo at [0, 0, 675]
+    veloPos[1] = 0; // velocity cross axis - y
+    veloPos[2] = (int) (lengthX - 1); // velocity inline axis - x
+    veloValue[0] = vmodelGrid.getAxisPhysicalOrigin(XAxis) + veloPos[2] * vmodelGrid.getAxisPhysicalDelta(XAxis);
+    veloValue[1] = vmodelGrid.getAxisPhysicalOrigin(YAxis) + veloPos[1] * vmodelGrid.getAxisPhysicalDelta(YAxis);
+    veloValue[2] = vmodelGrid.getAxisPhysicalOrigin(ZAxis) + veloPos[0] * vmodelGrid.getAxisPhysicalDelta(ZAxis);
 
-			// for Readability
-			out.println(" ");
-		}
-		
-		closeWriter();
-	}
+    out.println("[VelocityInformation]: " + "[s,t,f]: " + Arrays.toString(veloPos) + " \t[x, y, d]: "
+        + Arrays.toString(veloValue));
+
+    // Velo at [0, 675, 675]
+    veloPos[1] = (int) (lengthY - 1); // velocity cross axis - y
+    veloPos[2] = (int) (lengthX - 1); // velocity inline axis - x
+    veloValue[0] = vmodelGrid.getAxisPhysicalOrigin(XAxis)
+        + veloPos[2] * vmodelGrid.getAxisPhysicalDelta(XAxis);
+    veloValue[1] = vmodelGrid.getAxisPhysicalOrigin(YAxis)
+        + veloPos[1] * vmodelGrid.getAxisPhysicalDelta(YAxis);
+    veloValue[2] = vmodelGrid.getAxisPhysicalOrigin(ZAxis)
+        + veloPos[0] * vmodelGrid.getAxisPhysicalDelta(ZAxis);
+
+    out.println("[VelocityInformation]: " + "[s,t,f]: "
+    + Arrays.toString(veloPos) + " \t[x, y, d]: "
+        + Arrays.toString(veloValue));
+
+    out.println(" ");
+
+  }
+
+  private void initalizeWriter(){
+    File f = new File(originalPN);
+    if (f.exists() && !f.isDirectory()) {
+      f.delete();
+    }
+    try {
+      FileWriter fWrtr = new FileWriter(generateNextPath());
+      out = new PrintWriter(fWrtr);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  private void closeWriter(){
+    if (out != null) {
+      out.close();
+    }
+  }
+
+  // Write to external file
+  public void write() {
+    initalizeWriter();
+
+    // Write the Volume Information to the file
+    try {
+      vff = new VelocityModelFromFile(toolContext);
+      vff.open("r");
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    writeVelocityInformation(out);
+
+    while (ipio.hasNext()) {
+      ipio.next();
+      seismicInput.setVolumePosition(ipio.getFilePosition());
+
+      try {
+        ipio.read();
+      } catch (SeisException e) {
+        e.printStackTrace();
+      }
+
+      checkGrid = new CheckGrids(seismicInput, toolContext);
+
+      // Write the Grid Information to the file
+      writeGridInformation(out);
+
+      // for Readability
+      out.println(" ");
+    }
+
+    closeWriter();
+  }
 
 }
