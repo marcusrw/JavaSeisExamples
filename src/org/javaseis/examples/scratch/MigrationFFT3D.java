40d8684ab0287c3a605820e963e25133f0dc6cb8
Marcus Wilson <wilsonmr@ogeop.com>
2015-08-05 18:11:48 -0600

    Class stub for Migratable FFT3D object

--- src/org/javaseis/examples/scratch/MigrationFFT3D.java
+++ src/org/javaseis/examples/scratch/MigrationFFT3D.java
@@ -4,47 +4,51 @@ import beta.javaseis.distributed.DistributedArray;
 import beta.javaseis.fft.SeisFft3d;
 import beta.javaseis.parallel.IParallelContext;
 
+//I need to understand inheritance better to finish this.
+//Probably implement Transformable instead
 public class MigrationFFT3D extends SeisFft3d {
+
+  private boolean isTimeTransformed = false;
+  private boolean isSpaceTransformed = false;
 
   public MigrationFFT3D(int[] len, DistributedArray a) {
     super(len, a);
-    // TODO Auto-generated constructor stub
   }
 
   public MigrationFFT3D(DistributedArray a) {
     super(a);
-    // TODO Auto-generated constructor stub
   }
 
   public MigrationFFT3D(int[] len, float[] pad, DistributedArray a) {
     super(len, pad, a);
-    // TODO Auto-generated constructor stub
   }
 
   public MigrationFFT3D(int[] len, float[] pad, int[] isign, DistributedArray a) {
     super(len, pad, isign, a);
-    // TODO Auto-generated constructor stub
   }
 
   public MigrationFFT3D(IParallelContext pc, int[] len, float[] pad) {
     super(pc, len, pad);
-    // TODO Auto-generated constructor stub
   }
 
   public MigrationFFT3D(IParallelContext pc, int[] len, float[] pad, int[] isign) {
     super(pc, len, pad, isign);
-    // TODO Auto-generated constructor stub
   }
 
+  //Main constructor.  The others all call this with the rest of the parameters
+  //defaulted.
   public MigrationFFT3D(IParallelContext pc, int[] len, float[] pad,
       int[] isign, DistributedArray a) {
     super(pc, len, pad, isign, a);
-    // TODO Auto-generated constructor stub
   }
 
   public MigrationFFT3D(DistributedArray KyKxF, int[] isign) {
     super(KyKxF, isign);
-    // TODO Auto-generated constructor stub
+    setTransformed(true);
   }
 
+  private void setTransformed(boolean isTransformed) {
+    this.isTimeTransformed = isTransformed;
+    this.isSpaceTransformed = isTransformed;
+  }
 }
