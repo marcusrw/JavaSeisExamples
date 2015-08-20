735a3469291b54db2d84f00dfbb5b78c24ae1f53
SLAUC <slaven.lauc@ogeop.com>
2015-08-20 15:58:34 -0600

    GreenSource Bug Fix

--- src/org/javaseis/source/GreenSource.java
+++ src/org/javaseis/source/GreenSource.java
@@ -2,16 +2,11 @@ package org.javaseis.source;
 
 import org.javaseis.grid.ICheckedGrid;
 import org.javaseis.imaging.PhaseShiftFFT3D;
+import org.javaseis.volume.ISeismicVolume;
 
 import beta.javaseis.distributed.DistributedArray;
 
 public class GreenSource implements ISourceVolume {
-
-  @Override
-  public float[] covertPhysToArray(double[] sourceXYZ, ICheckedGrid CheckedGrid) {
-    // TODO Auto-generated method stub
-    return null;
-  }
 
   @Override
   public PhaseShiftFFT3D getShot() {
@@ -21,6 +16,18 @@ public class GreenSource implements ISourceVolume {
 
   @Override
   public DistributedArray getDistributedArray() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public float[] convertPhysToArray(double[] sourceXYZ, ISeismicVolume input) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public float[] covertPhysToArray(double[] sourceXYZ, ICheckedGrid CheckedGrid) {
     // TODO Auto-generated method stub
     return null;
   }
