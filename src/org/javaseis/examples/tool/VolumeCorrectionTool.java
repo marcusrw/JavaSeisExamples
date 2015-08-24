4fc3e7e2567746fb256a47b7b08ad91eed235038
SLAUC <slaven.lauc@ogeop.com>
2015-08-24 09:58:06 -0600

    Virtual trace properties set for output volume, coords accessing in following tools should work as expected

--- src/org/javaseis/examples/tool/VolumeCorrectionTool.java
+++ src/org/javaseis/examples/tool/VolumeCorrectionTool.java
@@ -4,10 +4,15 @@ import static org.javaseis.utils.Convert.listToArray;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.javaseis.examples.scratch.ExampleMigration;
 import org.javaseis.grid.GridDefinition;
 import org.javaseis.grid.JTestCheckedGridNew;
 import org.javaseis.properties.AxisDefinition;
@@ -21,7 +26,10 @@ import org.javaseis.util.SeisException;
 import org.javaseis.volume.ISeismicVolume;
 
 import beta.javaseis.array.ITraceIterator;
+import beta.javaseis.distributed.FileSystemIOService;
+import beta.javaseis.distributed.IDistributedIOService;
 import beta.javaseis.parallel.IParallelContext;
+import beta.javaseis.parallel.UniprocessorContext;
 import beta.javaseis.services.IPropertyService;
 import beta.javaseis.services.VolumePropertyService;
 import edu.mines.jtk.util.ArrayMath;
@@ -37,51 +45,77 @@ import org.junit.Assert;
  * be multiplied with input data to produce output data
  */
 public class VolumeCorrectionTool implements IVolumeTool {
+
+  private static final Logger LOGGER = Logger.getLogger(VolumeCorrectionTool.class.getName());;
+
   private static final long serialVersionUID = 1L;
 
-  int Xindex = 2;
-  int Yindex = 1;
-  int Zindex = 0;
+  IDistributedIOService ipio;
+  VolumePropertyService vps;
+  boolean usesProperties;
+  String inputFileSystem, inputFileName;
 
   @Override
-  public void serialInit(ToolState toolState) {
-    // Get the scalar multiplier
-    // scalarValue = toolState.getFloatParameter("scalarValue", 0f);
-    // toolState.log("Scalar value for multiplication: " + scalarValue);
+  public void serialInit(ToolState toolState) throws SeisException {
+    inputFileSystem = toolState.getParameter(ToolState.INPUT_FILE_SYSTEM);
+    toolState.log("Input file system: " + inputFileSystem);
+    inputFileName = toolState.getParameter(ToolState.INPUT_FILE_NAME);
+    toolState.log("Input file name: " + inputFileName);
+    IParallelContext upc = new UniprocessorContext();
+    ipio = new FileSystemIOService(upc, inputFileSystem);
+    ipio.open(inputFileName);
+    toolState.log("Opened file in serial mode");
+    // toolState.setOutputState(new DataState(ipio,
+    // toolState.getIntParameter(ToolState.TASK_COUNT, 1)));
+    ipio.close();
   }
 
   @Override
-  public void parallelInit(IParallelContext pc, ToolState toolState) {
-    // pc.serialPrint("Entering parallelInit: scalarValue = " + scalarValue);
+  public void parallelInit(IParallelContext pc, ToolState toolState) throws SeisException {
+    ipio = new FileSystemIOService(pc, inputFileSystem);
+    ipio.open(inputFileName);
+    ISeismicVolume outputVolume = (ISeismicVolume) toolState.getObject(ToolState.OUTPUT_VOLUME);
+    ipio.setSeismicVolume(outputVolume);
+    ipio.reset();
+    /*
+     * if (ipio.usesProperties()) { vps = (VolumePropertyService)
+     * (ipio.getPropertyService()); pc.masterPrint("\n" + vps.listProperties() +
+     * "\n"); }
+     */
+    pc.serialPrint("Re-opened file in parallel mode");
   }
 
   // Adjust the new Physical Delta
   private double CalculateNewDeltaOrigin(AxisDefinition axis, int k, double[] data) {
-    if (k == Xindex) {
-      return data[0];
-    } else if (k == Yindex) {
-      return data[1];
-    } else {
+    if (k == 0) {
       if (data[2] == 0) {
-        // In a synthetic data set the physical delta on the Z axis can be 0
-        return 0.008;
+        // In a synthetic data set the physical delta on the Z axis can
+        // be 0
+        return 0.000001;
       }
       return data[2];
+    } else if (k == 1) {
+      return data[1];
+    } else if (k == 2) {
+      return data[0];
     }
+    return Double.MAX_VALUE;
   }
 
   // Adjust the new Physical Origin
   private double CalculateNewPhysicalOrigin(AxisDefinition axis, int k, double[] data) {
-    if (k == Xindex) {
+    if (k == 2) {
       return data[0];
-    } else if (k == Yindex) {
+    } else if (k == 1) {
       return data[1];
-    } else {
+    } else if (k == 0) {
       return data[2];
     }
+    return Double.MAX_VALUE;
   }
 
   private GridDefinition updateVolumeGridDefinition(ISeismicVolume input, ToolState toolContext) {
+
     GridDefinition inputGrid = input.getGlobalGrid();
     long[] inputAxisLengths = inputGrid.getAxisLengths();
 
@@ -95,10 +129,9 @@ public class VolumeCorrectionTool implements IVolumeTool {
 
     input.getCoords(position, srcXYZ, recXYZ);
 
-    // System.out.println("[PV updateVolumeGridDefinition] Source: " +
-    // Arrays.toString(srcXYZ));
-    // System.out.println("[PV updateVolumeGridDefinition] Reciever: " +
-    // Arrays.toString(recXYZ));
+    //LOGGER.info("[PV updateVolumeGridDefinition] Source: " + Arrays.toString(srcXYZ));
+
+    //LOGGER.info("[PV updateVolumeGridDefinition] Reciever: " + Arrays.toString(recXYZ));
 
     int[] position2 = new int[] { 0, 0, 0 };
 
@@ -108,31 +141,24 @@ public class VolumeCorrectionTool implements IVolumeTool {
     double[] srcXYZ2 = new double[3];
     double[] recXYZ2 = new double[3];
 
-    // System.out.println(Arrays.toString(position2));
-
     input.getCoords(position2, srcXYZ2, recXYZ2);
 
-    // System.out.println("[PV updateVolumeGridDefinition] Source 2: " +
-    // Arrays.toString(srcXYZ2));
-    // System.out.println("[PV updateVolumeGridDefinition] Reciever 2: " +
-    // Arrays.toString(recXYZ2));
+    //LOGGER.info("[PV updateVolumeGridDefinition] Source: " + Arrays.toString(srcXYZ2));
+
+    //LOGGER.info("[PV updateVolumeGridDefinition] Reciever: " + Arrays.toString(recXYZ2));
 
     for (int k = 0; k < recXYZ2.length; k++) {
       recXYZ2[k] -= recXYZ[k];
     }
 
-    // System.out.println("[PV updateVolumeGridDefinition] Reciever Delta: " +
-    // Arrays.toString(recXYZ2));
+    //LOGGER.info("[PV updateVolumeGridDefinition] Reciever: " + Arrays.toString(recXYZ));
+
+    //LOGGER.info("[PV updateVolumeGridDefinition] Delta: " + Arrays.toString(recXYZ2));
 
     for (int gridDefIndex = 0; gridDefIndex < inputAxisLengths.length; gridDefIndex++) {
       AxisDefinition inputAxis = inputGrid.getAxis(gridDefIndex);
-      physicalOAxisArray[gridDefIndex] = new AxisDefinition(
-          inputAxis.getLabel(),
-          inputAxis.getUnits(),
-          inputAxis.getDomain(),
-          inputAxis.getLength(),
-          inputAxis.getLogicalOrigin(),
-          inputAxis.getLogicalDelta(),
+      physicalOAxisArray[gridDefIndex] = new AxisDefinition(inputAxis.getLabel(), inputAxis.getUnits(),
+          inputAxis.getDomain(), inputAxis.getLength(), inputAxis.getLogicalOrigin(), inputAxis.getLogicalDelta(),
           CalculateNewPhysicalOrigin(inputAxis, gridDefIndex, recXYZ),
           CalculateNewDeltaOrigin(inputAxis, gridDefIndex, recXYZ2));
     }
@@ -163,52 +189,46 @@ public class VolumeCorrectionTool implements IVolumeTool {
   }
 
   @Override
-  public boolean processVolume(IParallelContext pc, ToolState toolState, ISeismicVolume input, ISeismicVolume output) {
-    // Get trace iterators for input and output
-
-    //output.copyVolume(input);
-
-    output.setPropertyService(input.getPropertyService());
-    IPropertyService propsInput = input.getPropertyService();
-    IPropertyService propsOutput = output.getPropertyService();
-    // Set the actual Properties of the actual traces
-    Assert.assertNotNull(propsInput);
-
-    //TODO not a proper copy
-    //Assert.assertNotEquals("Input and output VolumeProperties are " +
-    //    "the same object, not clones.",propsInput,propsOutput);
+  public boolean processVolume(IParallelContext pc, ToolState toolState, ISeismicVolume input, ISeismicVolume output)
+      throws SeisException {
 
     GridDefinition inputGrid = updateVolumeGridDefinition(input, toolState);
+
+    // Create a new Instance of VolumePropertyService
+    VolumePropertyService vpsO = new VolumePropertyService(ipio);
+
+    output.setPropertyService(vpsO);
+
+    VolumePropertyService propsInput = (VolumePropertyService) input.getPropertyService();
+
+    VolumePropertyService propsOutput = (VolumePropertyService) output.getPropertyService();
+
+    // Set the actual Properties of the actual traces
+    // Input should never be null
+    Assert.assertNotNull(propsInput);
 
     ITraceIterator iti = input.getTraceIterator();
     ITraceIterator oti = output.getTraceIterator();
 
     GridDefinition outputGrid = null;
 
-    // Remove Eventually
     double traceAxisMin = (double) Double.MAX_VALUE - 1;
     double frameAxisMin = (double) Double.MAX_VALUE - 1;
     double traceDelta = (double) Double.MAX_VALUE - 1;
     double frameDelta = (double) Double.MAX_VALUE - 1;
-    // End
 
     while (iti.hasNext()) {
 
       iti.next();
 
       // Clone the Trace
-      float[] actualTrace = iti.getTrace().clone();
+      float[] actualTrace = iti.getTrace();
 
       int[] tracePos = iti.getPosition().clone();
 
-      //System.out.println("Input Iterator Pos: " + Arrays.toString(tracePos));
+      LOGGER.info("Input Iterator Pos: " + Arrays.toString(tracePos));
 
-      // Get rec position
-      double[] srcXYZ = new double[3];
-      double[] recXYZ = new double[3];
-      input.getCoords(tracePos, srcXYZ, recXYZ);
-
-      // System.out.println("Reciever Cords: " + Arrays.toString(recXYZ));
+      propsInput.setPosition(tracePos);
 
       int traceIndex = tracePos[1];
       int frameIndex = tracePos[2];
@@ -218,12 +238,10 @@ public class VolumeCorrectionTool implements IVolumeTool {
 
       double tracePhysicalOrigin = inputGrid.getAxis(1).getPhysicalOrigin();
       double framePhyscialOrigin = inputGrid.getAxis(2).getPhysicalOrigin();
-      // Move Here
-      // double traceDelta = inputGrid.getAxis(1).getPhysicalDelta();
-      // double frameDelta = inputGrid.getAxis(2).getPhysicalDelta();
+
       traceDelta = inputGrid.getAxis(1).getPhysicalDelta();
       frameDelta = inputGrid.getAxis(2).getPhysicalDelta();
-      // end
+
       double traceAxisMinAccum;
       double frameAxisMinAccum;
 
@@ -242,9 +260,6 @@ public class VolumeCorrectionTool implements IVolumeTool {
         frameAxisMin = framePhyscialOrigin;
       }
 
-      //System.out.println("MinValue of Trace Axis: " + traceAxisMinAccum);
-      //System.out.println("MinValue of Frame Axis: " + frameAxisMinAccum);
-
       Double ftIndex = Math.abs((tracePhysicalOrigin - traceAxisMinAccum) / (traceDelta));
       Double ffIndex = Math.abs((framePhyscialOrigin - frameAxisMinAccum) / (frameDelta));
 
@@ -252,45 +267,83 @@ public class VolumeCorrectionTool implements IVolumeTool {
       Integer ffIndexInt = (int) Math.round(ffIndex);
 
       int[] finalPosition = new int[] { 0, ftIndexInt, ffIndexInt };
-      // System.out.println("Volume Shape: " +
-      // Arrays.toString(output.getVolumeShape()));
-      // System.out.println("Itr Shape: " + Arrays.toString(oti.getShape()));
-      //System.out.println("Final Trace Location: " + Arrays.toString(finalPosition));
-      oti.next();
+
+      // Don't want to say this is not how it should work
+      // but set position should internally call next() in order
+      // to put it the itrator at the right location.
+      // TODO: Change the way trace itr works
       oti.setPosition(finalPosition);
+      oti.next();
+
+      LOGGER.info("Output Trace Location: " + Arrays.toString(oti.getPosition().clone()));
+
       oti.putTrace(actualTrace);
 
-      // Assert.assertNotNull(propsOutput);
-      // propsInput.copyTrcProps(propsInput, tracePos);
-      // propsOutput;
-      try {
-       // propsOutput.copyTrcProps(propsInput, tracePos, finalPosition);
-      } catch (IllegalArgumentException e) {
-        System.out.println(Arrays.toString(tracePos));
-        System.out.println(Arrays.toString(finalPosition));
-        throw e;
-      }
-      // System.out.println("Output Itr Pos: " +
-      // Arrays.toString(oti.getPosition().clone()));
+      Assert.assertNotNull(propsOutput);
 
-      // try { Thread.sleep(5000); } catch (InterruptedException e) { // TODO
-      // Auto-generated catch block e.printStackTrace();
+      propsOutput.setPosition(finalPosition);
+
+      double[] sc = new double[3];
+      double[] rc = new double[3];
+      output.getCoords(finalPosition, sc, rc);
+
+      LOGGER.info("Before Trace Copy: " + Arrays.toString(rc));
+
+      try {
+        propsOutput.copyTrcProps(propsInput, tracePos, finalPosition);
+      } catch (IllegalArgumentException e) {
+        LOGGER.log(Level.SEVERE, e.getMessage(), e);
+      }
+
+      LOGGER.info("Before Trace Copy: " + Arrays.toString(rc));
+
+      output.getCoords(finalPosition, sc, rc);
+
+      LOGGER.info("After Trace Copy: " + Arrays.toString(rc));
+
+      //Testing Sleep Function
+      // try {
+      // Thread.sleep(2500);
+      // } catch (InterruptedException e) {
       // }
 
     }
-
-    // System.out.println(Arrays.toString(output.getVolumeShape()));
+    
     double[] minPhys = new double[] { 0, traceAxisMin, frameAxisMin };
     double[] Delta = new double[] { 0.00001, traceDelta, frameDelta };
+
+    int[] posTest1 = new int[] { 0, 0, 0 };
+    
+    LOGGER.info("ATTEMPTING TO GET POS: " + Arrays.toString(posTest1));
+    
+    double[] sr = new double[3];
+    double[] rc = new double[3];
+
+    output.getCoords(posTest1, sr, rc);
+
+    LOGGER.info("Source Trace: " + Arrays.toString(sr));
+    LOGGER.info("Reciever Trace: " + Arrays.toString(rc));
+
+    int[] posTest2 = new int[] { 0, 200, 200 };
+    
+    LOGGER.info("ATTEMPTING TO GET POS: " + Arrays.toString(posTest2));
+
+    output.getCoords(posTest2, sr, rc);
+
+    LOGGER.info("Source Trace: " + Arrays.toString(sr));
+    LOGGER.info("Reciever Trace: " + Arrays.toString(rc));
 
     outputGrid = generateOutputGrid(input, minPhys, Delta);
     setOutgoingDataStateGrid(toolState, outputGrid);
 
+    if (ipio.hasNext()) {
+      ipio.next();
+    }
+
     return true;
   }
 
-  private void setOutgoingDataStateGrid(ToolState toolState,
-      GridDefinition outputGrid) {
+  private void setOutgoingDataStateGrid(ToolState toolState, GridDefinition outputGrid) {
     toolState.getOutputState().gridDefinition = outputGrid;
     DataState outputState = toolState.getOutputState();
     outputState.gridDefinition = outputGrid;
@@ -313,12 +366,24 @@ public class VolumeCorrectionTool implements IVolumeTool {
     // Nothing to clean up in serial mode
   }
 
+  private void writeObject(ObjectOutputStream oos) throws IOException {
+    // default serialization
+    oos.writeObject(inputFileSystem);
+    oos.writeObject(inputFileName);
+  }
+
+  private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
+    // default de-serialization
+    inputFileSystem = (String) ois.readObject();
+    inputFileName = (String) ois.readObject();
+  }
+
   private static ParameterService basicParameters() {
-    String inputFileName = "seg45shot.js";
+    String inputFileName = "segshotno1.js";
     String outputFileName = "fishfish.js";
     ParameterService parms = null;
     try {
-      parms = new FindTestData(inputFileName,outputFileName).getParameterService();
+      parms = new FindTestData(inputFileName, outputFileName).getParameterService();
     } catch (FileNotFoundException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
@@ -334,7 +399,7 @@ public class VolumeCorrectionTool implements IVolumeTool {
 
     toolList.add(ExampleVolumeInputTool.class.getCanonicalName());
     toolList.add(VolumeCorrectionTool.class.getCanonicalName());
-    toolList.add(ExampleVolumeOutputTool.class.getCanonicalName());
+    toolList.add(VolumeToolCheckerTool.class.getCanonicalName());
 
     String[] toolArray = listToArray(toolList);
 
